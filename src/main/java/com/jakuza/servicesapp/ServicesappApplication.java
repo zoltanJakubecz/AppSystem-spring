aa5271b3771b68619d05be35a81e288f623ecb01
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-17 08:26:12 +0100

    Initial commit

---
+++ src/main/java/com/jakuza/servicesapp/ServicesappApplication.java
@@ -0,0 +1,13 @@
+package com.jakuza.servicesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jakuza/servicesapp/ServicesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakuza.servicesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
