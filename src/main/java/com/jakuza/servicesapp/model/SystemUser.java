e2f7a6a83349e40b509029ea7f0d5055f6bad54a
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-17 10:07:36 +0100

    Add user class

---
+++ src/main/java/com/jakuza/servicesapp/model/SystemUser.java
@@ -0,0 +1,13 @@
+package com.jakuza.servicesapp.model;
+
+import java.util.List;
+import java.util.UUID;
+
+public class SystemUser {
+
+    private UUID id;
+
+    private String fullName;
+
+    private List<System> systems;
+}
