17eb490e3b779370a84cd761e28025cf32dc2323
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-18 20:17:15 +0100

    add update and delete user route

--- src/main/java/com/jakuza/servicesapp/controller/AppUserController.java
+++ src/main/java/com/jakuza/servicesapp/controller/AppUserController.java
@@ -4,9 +4,9 @@ import com.jakuza.servicesapp.model.dto.AppUserDto;
 import com.jakuza.servicesapp.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +16,18 @@ public class AppUserController {
 
     @PostMapping("/")
     public ResponseEntity<AppUserDto> create(@RequestBody AppUserDto userDto){
-        return ResponseEntity.ok().body(service.createAppUser(userDto));
+        return ResponseEntity.ok().body(service.create(userDto));
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AppUserDto> update(@PathVariable("id") UUID id, @RequestBody AppUserDto userDto){
+        return ResponseEntity.ok().body(service.update(id, userDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteMovie(@PathVariable("id") UUID id) {
+        return service.remove(id);
+    }
+
 
 }
