d7d0363a02bd29c17b395406dcad93ec2dccbfd8
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-18 20:44:21 +0100

    refactor app name

--- src/main/java/com/jakuza/servicesapp/ServicesAppApplication.java
+++ src/main/java/com/jakuza/servicesapp/ServicesAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServicesappApplication {
+public class ServicesAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServicesappApplication.class, args);
+        SpringApplication.run(ServicesAppApplication.class, args);
     }
 
 }
