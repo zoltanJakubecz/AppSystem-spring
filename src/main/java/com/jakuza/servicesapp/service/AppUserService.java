efc2098e2aa6c29b8a0781d4d8ee6fe848eaa9d8
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-18 10:49:29 +0100

    add update and delete method to user service

--- src/main/java/com/jakuza/servicesapp/service/AppUserService.java
+++ src/main/java/com/jakuza/servicesapp/service/AppUserService.java
@@ -6,15 +6,37 @@ import com.jakuza.servicesapp.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class AppUserService {
     private final AppUserRepository userRepository;
 
-    public AppUserDto createAppUser(AppUserDto appUserDto){
+    public AppUserDto create(AppUserDto appUserDto){
         return AppUserDto.fromEntity(userRepository.save(AppUser.builder()
                 .fullName(appUserDto.getFullName())
                 .build()));
+    }
+
+    @Transactional
+    public AppUserDto update(UUID id, AppUserDto user){
+        AppUser toUpdate = userRepository.findById(id).orElse(null);
+        if(toUpdate == null) return null;
+        toUpdate.setFullName(user.getFullName());
+        return AppUserDto.fromEntity(userRepository.save(AppUser.builder()
+                .fullName(toUpdate.getFullName())
+                .build()));
+    }
+
+    public boolean remove(UUID id){
+        AppUser toRemove = userRepository.findById(id).orElse(null);
+        if(toRemove != null) {
+            userRepository.delete(toRemove);
+            return true;
+        }
+        return false;
     }
 
 }
