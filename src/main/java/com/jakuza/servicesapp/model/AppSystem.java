dec4c8169b1043c76d6ce0b563575c34f9e265f4
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-18 20:18:12 +0100

    add list handlings to appSystem class

--- src/main/java/com/jakuza/servicesapp/model/AppSystem.java
+++ src/main/java/com/jakuza/servicesapp/model/AppSystem.java
@@ -3,6 +3,7 @@ package com.jakuza.servicesapp.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,5 +27,19 @@ public class AppSystem {
     @ManyToMany
     private List<MenuItem> mainMenu;
 
+    public void addUserToSystem(AppUser user){
+        if(appUserList == null) appUserList = new ArrayList<>();
+        appUserList.add(user);
+    }
+
+    public void addApplication(Application application){
+        if(applicationList == null) applicationList = new ArrayList<>();
+        applicationList.add(application);
+    }
+
+    public void addMenuItem(MenuItem menuItem){
+        if(mainMenu == null) mainMenu = new ArrayList<>();
+        this.mainMenu.add(menuItem);
+    }
 
 }
