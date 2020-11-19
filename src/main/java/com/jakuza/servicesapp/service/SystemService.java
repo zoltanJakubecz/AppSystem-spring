100a249b71d4f4532027c6982599336526e2c026
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-19 17:53:50 +0100

    add update method to SystemService

--- src/main/java/com/jakuza/servicesapp/service/SystemService.java
+++ src/main/java/com/jakuza/servicesapp/service/SystemService.java
@@ -1,9 +1,11 @@
 package com.jakuza.servicesapp.service;
 
-import com.jakuza.servicesapp.model.MenuItem;
+import com.jakuza.servicesapp.model.AppSystem;
 import com.jakuza.servicesapp.repository.AppSystemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +13,10 @@ public class SystemService {
 
     private final AppSystemRepository repository;
 
-    public void addMenu(MenuItem item){
-
+    @Transactional
+    public void update(AppSystem system){
+        AppSystem appSystem = repository.findById(system.getId()).orElse(null);
+        if(appSystem == null) return;
+        appSystem.setMainMenu(system.getMainMenu());
     }
-
 }
