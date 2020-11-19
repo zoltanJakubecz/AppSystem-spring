b9bc42aa984f075408fe084afdbbe51a33e39018
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-19 20:49:41 +0100

    implement icon service create update delete

--- src/main/java/com/jakuza/servicesapp/service/IconService.java
+++ src/main/java/com/jakuza/servicesapp/service/IconService.java
@@ -1,11 +1,13 @@
 package com.jakuza.servicesapp.service;
 
-import com.jakuza.servicesapp.model.AppUser;
 import com.jakuza.servicesapp.model.Icon;
 import com.jakuza.servicesapp.model.dto.IconDto;
 import com.jakuza.servicesapp.repository.IconRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +20,23 @@ public class IconService {
                 .name(iconDto.getName())
                 .application(iconDto.getApplication())
                 .build()));
+    }
+
+    @Transactional
+    public IconDto update(IconDto iconDto){
+        Icon toUpdate = iconRepository.findById(iconDto.getId()).orElse(null);
+        if(toUpdate == null) return null;
+        toUpdate.setName(iconDto.getName());
+        toUpdate.setApplication(iconDto.getApplication());
+        return IconDto.fromEntity(toUpdate);
+    }
+
+    public boolean remove(UUID id){
+        Icon toRemove = iconRepository.findById(id).orElse(null);
+        if(toRemove != null) {
+            iconRepository.delete(toRemove);
+            return true;
+        }
+        return false;
     }
 }
